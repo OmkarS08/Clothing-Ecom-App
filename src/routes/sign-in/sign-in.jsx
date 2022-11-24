de722a2c71d8a1709e7b8a8db548fa40d2b1482b
omkar shelke <omkar1144@gmail.com>
2022-11-24 15:08:10 +0400

    adding users -> Db

--- src/routes/sign-in/sign-in.jsx
+++ src/routes/sign-in/sign-in.jsx
@@ -1,11 +1,11 @@
-import { PopUpSignIn } from "../../utils/firebase.utils";
+import { PopUpSignIn , createUserDocumentFromAuth} from "../../utils/firebase.utils";
 
 
 
 const SignIn =() =>{
     const logUser = async() =>{
-        const responce = await PopUpSignIn();
-        console.log(responce);
+        const {user} = await PopUpSignIn();
+        createUserDocumentFromAuth(user);
     }
     return(<div>This is Sign page
     <button onClick={logUser}>Google</button></div>)
--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth ,signInWithPopup,signInWithRedirect ,GoogleAuthProvider} from 'firebase/auth'
+import {doc,setDoc,getDoc,getFirestore, Firestore} from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,6 +15,7 @@ const firebaseConfig = {
   appId: "1:835752965093:web:ca33f5aee18458bcc5350c"
 };
 
+
 // Initialize Firebase
 const FireBaseApp = initializeApp(firebaseConfig);
 
@@ -25,3 +27,34 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const PopUpSignIn = () => signInWithPopup(auth ,provider)
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth =  async (userAuth) =>{
+    const userDocRef = doc(db,'users',userAuth.uid);
+    console.log(userDocRef);
+    const  userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+ // user data exists?
+// creating user data
+if(!userSnapshot.exists())
+{
+    const {email , displayName } =userAuth;
+    const createdAt = new Date();
+    try {
+        await setDoc(userDocRef,{
+            displayName,
+            email,
+            createdAt
+        });
+        
+    } catch (error) {
+        console.log('Error!-created during adding users', error.message)
+    }
+}
+return userDocRef;
+ 
+ // retrun user Document
+}
