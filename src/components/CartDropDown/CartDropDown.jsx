9cea5227d1506671609f8bccfac13de2d74c469f
omkar shelke <omkar1144@gmail.com>
2022-12-11 22:31:02 +0400

    add to cart done

--- src/components/CartDropDown/CartDropDown.jsx
+++ src/components/CartDropDown/CartDropDown.jsx
@@ -1,12 +1,27 @@
 import './CartDropDown.scss'
-import Button from '../Button/Button'
-import { CartItem } from '../CartItem/CartItem'
-import { useContext } from 'react'
-import { CartContext } from '../../contexts/Card.context'
+import { useContext } from 'react';
 
-export const CartDropDown =()=>{
-    return(<div className='cart-dropdown-container'>
-        <div className='cart-items' />
-        <Button>Checkout</Button>
-        </div>)
-}
+import { CartContext } from '../../contexts/Card.context';
+import Button from '../Button/Button'
+import { CartItem } from '../CartItem/CartItem';
+import './CartDropDown.scss'
+
+export const CartDropDown = () => {
+  const { cartItems } = useContext(CartContext);
+
+  return (
+    <div className='cart-dropdown-container'>
+      <div className='cart-items'>
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <span className='empty-message'>Your cart is empty</span>
+        )}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
+
