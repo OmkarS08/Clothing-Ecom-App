3ee5d0434244b540e5a3e06e04206eb641713bba
omkar shelke <omkar1144@gmail.com>
2022-12-11 21:12:40 +0400

    cart -item

---
+++ src/components/CartItem/CartItem.jsx
@@ -0,0 +1,10 @@
+import './CartItem.scss'
+
+export const CartItem =({cartItem}) =>{
+    
+    const {name,quantity}=cartItem;
+    return(<div>
+        <h2>{name}</h2>
+        <span>{quantity}</span>
+    </div>)
+}
